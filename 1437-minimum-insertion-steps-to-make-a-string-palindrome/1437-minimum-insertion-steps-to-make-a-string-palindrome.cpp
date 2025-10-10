1f652641b4ff676d8793626517ef763b9ce3ca7d
MikeAlpha0612 <michaeljai0612@gmail.com>
2025-10-10 17:28:12 +0530

    Time: 32 ms (59.97%), Space: 31.6 MB (39.29%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,42 +1,31 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int result(std::string& s, std::string& str, int n1, int n2, std::vector<std::vector<int>>& dp) {
-        // Base case: if one string is empty, we must insert/delete all
-        // characters of the other string.
-        if (n1 < 0) return n2 + 1;
-        if (n2 < 0) return n1 + 1;
 
-        if (dp[n1][n2] != -1) {
-            return dp[n1][n2];
-        }
+    int result(string& s, int n, int i, int j, vector<vector<int>>& dp ){
+      
+      if(i >= j){
+        return 0;
+      }
 
-        // If characters match, no operation is needed for them.
-        if (s[n1] == str[n2]) {
-            return dp[n1][n2] = result(s, str, n1 - 1, n2 - 1, dp);
-        }
+      if(dp[i][j] != -1) return dp[i][j];
 
-        // If they don't match, we pay a cost of 1 and take the minimum of
-        // deleting from s (n1-1) or inserting into s (n2-1).
-        return dp[n1][n2] = 1 + std::min(result(s, str, n1 - 1, n2, dp), 
-                                         result(s, str, n1, n2 - 1, dp));
+      if(s[i] == s[j]){
+        return dp[i][j] = result(s, n, i+1, j-1, dp);
+      }
+
+      return dp[i][j] = 1 + min(result(s,n, i+1, j,dp), result(s,n, i, j-1, dp));
+
     }
 
-    int minInsertions(std::string s) {
+    int minInsertions(string s) {
         int n = s.size();
-        
-        // FIX: Declare str as a string and reverse efficiently
-        std::string str = s;
-        std::reverse(str.begin(), str.end());
+    
 
-        std::vector<std::vector<int>> dp(n, std::vector<int>(n, -1));
+        vector<vector<int>>dp(n+1, vector<int>(n+1,-1));
 
-        int edit_distance = result(s, str, n - 1, n - 1, dp);
+        int res = result(s, n, 0, n-1, dp);
 
-        // FIX: The answer is the calculated edit distance divided by 2
-        return edit_distance / 2;
+        return res;
+
     }
 };
